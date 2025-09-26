39554add22b7aa2540d34e7cf40220bdd220f6aa
Victor Nyberg <contact@victornyberg.com>
2025-09-27 07:45:01 +1000

    Revert singla store example to livestoreprovider

--- playground/src/pages/MultipleStoresPage.vue
+++ playground/src/pages/MultipleStoresPage.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { TodoProvider as TodoStoreProvider, useTodoStore } from '../livestore/todos/store'
+import { TodoProvider } from '../livestore/todos/store'
 import ToDos from '../components/to-dos.vue'
 </script>
 
 <template>
   <div>
-    <TodoStoreProvider storeId="store-1">
+    <TodoProvider storeId="store-1">
       <template #loading>Loading store-1...</template>
       <template #default>
         <h3>Store 1</h3>
         <ToDos />
       </template>
-    </TodoStoreProvider>
+    </TodoProvider>
 
-    <TodoStoreProvider storeId="store-2">
+    <TodoProvider storeId="store-2">
       <template #loading>Loading store-2...</template>
       <template #default>
         <h3>Store 2</h3>
         <ToDos />
       </template>
-    </TodoStoreProvider>
+    </TodoProvider>
   </div>
 </template>
--- playground/src/pages/SingleStorePage.vue
+++ playground/src/pages/SingleStorePage.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import { TodoProvider as StoreProvider, useTodoStore as useStore } from '../livestore/todos/store'
+import { LiveStoreProvider } from 'vue-livestore'
+import { makePersistedAdapter } from '@livestore/adapter-web'
+import LiveStoreWorker from '../livestore/todos/livestore.worker?worker'
+import LiveStoreSharedWorker from '@livestore/adapter-web/shared-worker?sharedworker'
+import { schema } from '../livestore/todos/schema'
 import ToDos from '../components/to-dos.vue'
+
+const adapter = makePersistedAdapter({
+  storage: { type: 'opfs' },
+  worker: LiveStoreWorker,
+  sharedWorker: LiveStoreSharedWorker,
+})
+
+const storeOptions = {
+  schema,
+  adapter,
+  storeId: 'todo-store-single',
+}
 </script>
 
 <template>
-  <StoreProvider storeId="todo-store">
+  <LiveStoreProvider :options="storeOptions">
     <template #loading>Loading...</template>
     <ToDos />
-  </StoreProvider>
+  </LiveStoreProvider>
 </template>
